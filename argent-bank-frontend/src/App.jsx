3430ff17435aa0944c5a47d055d6690611aaac5f
Anastasia said <anastasiasaidfara@gmail.com>
2026-04-21 17:52:46 +0200

    environnement et route react cree

---
+++ argent-bank-frontend/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
+import Home from './Pages/Home';
+import Error from './Pages/Error';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
+  )
+}
+
+export default App
